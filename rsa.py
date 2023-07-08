610721ede0e204563990a469e11ce06d2066ba27
ViniciusGN <170115437@aluno.unb.br>
2023-07-08 11:01:41 -0300

    change

--- rsa.py
+++ rsa.py
@@ -1,6 +1,9 @@
 from random import randrange
 import random
+import secrets
+import hashlib
 
+##########################################################################  RSA Core Functions:
 def rsa_gerador_primo():
     number_e_primo = False
     k = 128 # número de rodadas de teste
@@ -51,7 +54,8 @@ def rsa_gerador_primo():
             number_e_primo = True
 
     return number
-        
+
+#--------------------------------------------------------RSA Generate Key:
 def rsa_generatekey(p, q):
 # Gerar um par de chaves RSA de dois números primos de 1024 bits
     l = []
@@ -64,13 +68,14 @@ def rsa_generatekey(p, q):
     phi = (p - 1) * (q - 1) # função totiente de Euler
     # A chave pública é um número e e tal que 1 < e < phi e mdc(e, phi) = 1
 
+#--------------------------------------------------------Teste de codigo:
     # Ocodigo abaixo foi abandonado, um numero de 1024 bit demorava uma eternidade...
     #...para processar por conta de um grande valor de RANGE. Usamos a função POW.
     # def modInverse(a, m): # Calcula o inverso modular de a mod m (inverso multiplicativo)
     #     for x in range(1, m):
     #         if ((a % m) * (x % m)) % m == 1:
     #             return x
-            
+ #--------------------------------------------------------Sub-function of RSA_generatekey:           
     def get_random_int(min, max):  # captura um número aleatório entre o min e o max
         return random.randint(min, max)
             
@@ -90,31 +95,104 @@ def rsa_generatekey(p, q):
     public_key = (number, e)
     private_key = (number, d)
     
-    print("\nChaves públicas (e=" + str(e) + ", n=" + str(number) + ")" + "\nChaves privadas (d=" + str(d) + ", n=" + str(number) + ")\n")
+    # print("\nChaves públicas (e=" + str(e) + ", n=" + str(number) + ")" + "\nChaves privadas (d=" + str(d) + ", n=" + str(number) + ")\n")
     return public_key, private_key
+#--------------------------------------------------------Teste de encriptação:
 
-def encripta_mensagem():
-    def criptografia(m, e, n):
-        c = (m**e) % n
-        return c
+# def encripta_mensagem():
+#     def criptografia(m, e, n):
+#         c = (m**e) % n
+#         return c
 
-    plain_text = input("Digite a mensagem: \t")
-    print('='*5 + ' Digite as chaves públicas: ' + '='*5)
-    e = int(input("Chave e:   "))
-    n = int(input("Chave n:   ")) 
-    enc = ''.join(chr(criptografia(ord(x), e, n)) for x in plain_text)
-    print('Texto Cifrado: ', enc, '\n')
-    return enc
+#     plain_text = input("Digite a mensagem: \t")
+#     print('='*5 + ' Digite as chaves públicas: ' + '='*5)
+#     e = int(input("Chave e:   "))
+#     n = int(input("Chave n:   ")) 
+#     enc = ''.join(chr(criptografia(ord(x), e, n)) for x in plain_text)
+#     print('Texto Cifrado: ', enc, '\n')
+#     return enc
 
+##########################################################################  Auxiliary Functions:
+def sha3_256(message1, message2=None):
+    if message2 is not None:
+        combined_value = str(message1) + str(message2)
+    else:
+        combined_value = str(message1)
+    sha3_hash = hashlib.sha3_256()
+    sha3_hash.update(combined_value.encode('utf-8'))
+    return sha3_hash.hexdigest()
+
+def convert_to_bits(n):
+    return [int(digit) for digit in bin(n)[2:]]
+
+def pad_bits(bits, length):
+    assert len(bits) <= length
+    return [0] * (length - len(bits)) + bits
+
+def xor(a, b):
+    assert len(a) == len(b) 
+    return bytes([x ^ y for x, y in zip(a, b)])
+
+def bits_to_string(b):
+    return ''.join(chr(int(''.join(x), 2)) for x in zip(*[iter(b)]*8))
+
+def mgf(data, length):
+    mgf_data = b''
+    counter = 0
+
+    while len(mgf_data) < length:
+        hash_input = data + counter.to_bytes(4, 'big')
+        hash_output = sha3_256(hash_input).digest()
+        mgf_data += hash_output
+        counter += 1
+
+    return mgf_data[:length]
+
+def generate_random_seed(length):
+    seed = secrets.token_bytes(length)
+    print("Semente aleatória:", seed.hex())
+    return seed
+
+
+##########################################################################  OAEP Functions:
+def oaep_pad(message, k):
+    # Etapa 1: Preenchimento do rótulo
+    label = b''
+    padded_message = message + (b'\x00' * (k - len(message) - len(label) - 1))
+    padded_message += b'\x01' + label
+
+    # Etapa 2: Geração do valor aleatório
+    seed = generate_random_seed(16)
+    seed_bits = int.from_bytes(seed, 'big')
+    seed = bin(seed_bits)[2:]  # Remover o prefixo '0b'
+
+    # Etapa 3: MGF (Mask Generation Function)
+    mask = mgf(seed, k - len(seed))
+
+    # Etapa 4: XOR com o valor aleatório
+    masked_message = bytes([padded_message[i] ^ mask[i] for i in range(len(mask))])
+
+    # Etapa 5: MGF (Mask Generation Function)
+    masked_seed = mgf(masked_message, len(seed))
+
+    # Etapa 6: XOR com a semente original
+    encoded_message = bytes([masked_message[i] ^ masked_seed[i] for i in range(len(masked_seed))])
+
+    return encoded_message
+
+##########################################################################  RSA Main:
 if __name__ == '__main__':
     p = rsa_gerador_primo()
     q = rsa_gerador_primo()
-    # q = rsa_gerador_primo()
-    print(p)
-    print("------------------")
-    print(q)
-    print("------------------")
 
     public_key, private_key = rsa_generatekey(p, q)
-    cypher_text = encripta_mensagem()
-    print(cypher_text)
+    print("Chaves geradas: ")
+    print(public_key)
+    print("------------------")
+    print(private_key)
+    print("------------------")
+
+    print('='*5 + ' Tamanho da chave = 1024 bits ' + '='*5)
+    print('='*5 + ' Digite o texto em claro: ' + '='*5)
+    plain_text = input("Digite a mensagem: \t")
+    print(oaep_pad(plain_text, 1024))

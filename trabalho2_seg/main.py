ef354bd58b406f97f6a181296346b30658d8e9ae
Adrianowiedmann <62907658+Adrianowiedmann@users.noreply.github.com>
2023-07-10 00:57:21 -0300

    Update main.py

--- trabalho2_seg/main.py
+++ trabalho2_seg/main.py
@@ -13,7 +13,9 @@ def autenticacao(mensagem):
     plaintext = padding_plaintext(plaintext)
     aes_key = lists_definition(aes_key)
     x = aes_encryption(aes_key, plaintext)
-    print(f"\nTexto cifrado: {x}")
+    x = [''.join(lista) for lista in x]
+    x = ''.join(x)
+    #print(f"\nTexto cifrado: {x}")
 
     rsa_public_key, rsa_private_key, signuture = rsa_operations('2', x)
     # a Assinatura é: x=AES_k(M), signuture=RSA_KA_s(H(AES_k(M))) e seguido das chaves RSA_KA_p e RSA_KA_s
@@ -28,7 +30,7 @@ def verificacao(n, e):
 
 if __name__ == '__main__':
     while(1):
-        print("\nOpções:\n1 - AES\n2 - RSA\n3 - Assinatura\n4 - Sair")
+        print("\nOpções:\n1 - AES\n2 - RSA\n3 - Sair")
         op = input("Escolha a opção: ")
         if (op == '1'):
             op_aes = 0
@@ -51,6 +53,7 @@ if __name__ == '__main__':
                 plaintext = padding_plaintext(plaintext)
                 key = lists_definition(key)
                 x = aes_encryption(key, plaintext)
+
             elif (op_aes == '2'):
                 text = input("Insira um texto cifrado (hexadecimal): ")
                 key = input("Insira a chave que foi usada para cifrar o texto (hexadecimal): ")
@@ -61,36 +64,7 @@ if __name__ == '__main__':
                 x = aes_decryption(key, text)
 
         elif op == '2':
-            op_rsa = 0
-            while op_rsa != '1' and op_rsa != '2' and op_rsa != '3':
-                print("\nOpções:\n1 - Cifração\n2 - Decifração\n3 - Sair")
-                op_rsa = input("Escolha a opção: ")
-            if (op_rsa == '1'):
-                rsa_operations('1')
-            elif (op_rsa == '2'):
-                n = input("Insira o n da chave privada: ")
-                d = input("Insira o d da chave privada: ")
-                e = ''
-                m = ''
-                rsa_operations('4', m, n, e, d)
+            rsa_operations('1')
 
         elif op == '3':
-            op_ass = 0
-            while op_ass != '1' and op_ass != '2' and op_ass != '3':
-                print("\nOpções:\n1 - Assinar\n2 - Verificar\n3 - Sair")
-                op_ass = input("Escolha a opção: ")
-            if (op_ass == '1'):
-                mensagem = input("Digite a mensagem a ser enviada: ")
-                sing = autenticacao(mensagem)
-                print("AES_k(M) - Mensagem Criptografada com AES: ", sing[0])
-                print("RSA_KA_s(H(AES_k(M))) - Assinatura: ", sing[1])
-                print("RSA_KA_p - Chave Pública RSA do emissor: ", sing[2])
-                print("-----Mantenha Segredo-----")
-                print("RSA_KA_s - Chave Privada RSA do emissor: ", sing[3])
-
-            elif (op_ass == '2'):
-                key = input("Insira a chave Púlbica RSA: ")
-                verificacao(key)
-
-        elif op == '4':
             break
